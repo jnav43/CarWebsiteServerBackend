25462fa07d4b843656a87337b09e40b5b3f4c471
jnav43 <jacobnavarro_95@yahoo.com>
2024-07-30 13:58:13 -0500

    Add files via upload

---
+++ pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
@@ -0,0 +1,32 @@
+package com.udacity.pricing.api;
+
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PriceException;
+import com.udacity.pricing.service.PricingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Implements a REST-based controller for the pricing service.
+ */
+@RestController
+@RequestMapping("/services/price")
+public class PricingController {
+
+    /**
+     * Gets the price for a requested vehicle.
+     *
+     * @param vehicleId ID number of the vehicle for which the price is requested
+     * @return price of the vehicle, or error that it was not found.
+     */
+    @GetMapping
+    public Price get(@RequestParam Long vehicleId) {
+        try {
+            return PricingService.getPrice(vehicleId);
+        } catch (PriceException priceException) {
+            return null;
+        }
+    }
+}
